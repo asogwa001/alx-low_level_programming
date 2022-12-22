9410c66df3f8995518c12563dc8b45fb92f5373e
asogwa emmanuel <asogwaemmanuel36@gmail.com>
2022-12-22 02:36:26 -0700

    init

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,6 +5,7 @@
 *Return:returns encoded string
 */
 
+/**
 char *leet(char *str)
 {
 	int index1 = 0, index2;
@@ -20,4 +21,40 @@ char *leet(char *str)
 		}
 	}
 	return (str);
+}
+*/
+char *leet(char *str){
+	int i = 0;
+	while(*(str + i))
+	{
+		if (*(str + i) == 'a' || *(str + i) == 'A')
+		{
+			*(str + i) = '4'; 
+
+		}
+		else if (*(str + i) == 'e' || *(str + i) == 'E')
+		{
+			*(str + i) = '3';
+
+		}
+		else if (*(str + i) == 'o' || *(str + i) == 'O')
+		{
+			*(str + i) = '0';
+
+		}
+		else if (*(str + i) == 't' || *(str + i) == 'T')
+		{
+			*(str + i) = '7';
+
+		}
+		else if (*(str + i) == 'l' || *(str + i) == 'L')
+		{
+			*(str + i) = '1';
+
+		}
+		i++;
+
+	}
+	return str;
+	
 }
